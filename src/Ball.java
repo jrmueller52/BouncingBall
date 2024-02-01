0cd8b7edaaa679ffd803bf5a03ab3cd29b058496
jillian.mueller24 <jrmueller52@gmail.com>
2024-02-01 11:25:48 -0600

    finished not angles

--- src/Ball.java
+++ src/Ball.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class Ball {
-}
+import java.awt.*;
+
+public class Ball {
+   private int size;
+   private Color color;
+   private int speedx;
+private int speedy;
+   private int x, y;
+   private MyPanel panel;
+
+   public Ball(MyPanel panel){
+       x = (int)(Math.random()*500+1);
+       y = (int)(Math.random()*500+1);
+       size = (int)(Math.random()*30+15);
+       speedx = (int)(Math.random()*6+2);
+       speedy = (int)(Math.random()*6+2);
+       color = new Color((int)(Math.random()*200+20),(int)(Math.random()*200+20),(int)(Math.random()*200+20));
+       this.panel = panel;
+   }
+
+   public void draw(Graphics g2){
+       g2.setColor(color);
+       g2.fillOval(x,y,size, size);
+   }
+
+   public void move(){
+      if((x>= panel.getWidth()-20) || (x<= 0)){
+          speedx = speedx*-1;
+           x+=speedx;
+           y+=speedy;
+      }
+      else if((y<=0) || (y>= panel.getHeight()-20)){
+
+          speedy = speedy*-1;
+          x+=speedx;
+          y+=speedy;
+      }
+
+      else{
+           x+=speedx;
+           y+=speedy;
+       }
+      }
+   }
+
+
--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class MyPanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class MyPanel extends JPanel {
+    Ball[] ballArray = new Ball[20];
+    public MyPanel(){
+
+        setBackground(Color.PINK);
+         for(int i = 0; i<20; i++){
+             ballArray[i] = new Ball(this);
+         }
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        for(int i = 0; i<20; i++) {
+            ballArray[i].draw(g);
+            ballArray[i].move();
+        }
+
+
+
+        try {
+            Thread.sleep(10);
+        }
+        catch(Exception e) {
+            System.out.println(e);
+        }
+            repaint();
+
+    }
+
+
 }
